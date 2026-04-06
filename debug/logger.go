afc22579edf695df1e1b75c4484a6d06ad87b5be
Harry Lawton <harrisonzimbo777@gmail.com>
2026-04-06 13:01:15 +0200

    logging

---
+++ debug/logger.go
@@ -0,0 +1,33 @@
+package debug
+
+import (
+	"log"
+	"os"
+)
+
+const logfile = "debug.log"
+
+var logger *log.Logger
+
+func Init() error {
+	f, err := os.OpenFile(logfile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	logger = log.New(f, "", log.Ltime|log.Lshortfile)
+	return nil
+}
+
+func Log(msg string) {
+	if logger == nil {
+		return
+	}
+	logger.Println(msg)
+}
+
+func Logf(format string, args ...any) {
+	if logger == nil {
+		return
+	}
+	logger.Printf(format, args...)
+}
